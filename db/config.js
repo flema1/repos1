538aa22b184bab5bfcaa4a69a6bccb20616e119a
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-07-29 19:55:22 -0400

    hello

--- db/config.js
+++ db/config.js
@@ -1,28 +1,15 @@
-const options = {
-  query: (e) => {
-    console.log(e.query);
-  }
-};
+const pgp = require('pg-promise')();
 
-const pgp = require('pg-promise')(options);
+let db;
 
-/**
- * Sets up the database with the proper database name and port
- * 
- * @returns a pg-promise instance
- */
-function setDatabase() {
-  if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-    return pgp({
-      database: 'delorean_movies_dev',
-      port: 5432,
-      host: 'localhost',
-    });
-  } else if (process.env.NODE_ENV === 'production') {
-    return pgp(process.env.DATABASE_URL);
-  }
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+  db = pgp({
+    database: 'adaquote_development',
+    port: 5432,
+    host: 'localhost',
+  });
+} else if (process.env.NODE_ENV === 'production') {
+  db = pgp(process.env.DATABASE_URL);
 }
-
-const db = setDatabase();
 
 module.exports = db;
